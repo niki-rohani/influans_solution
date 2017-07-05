39e0552c9ca609c9e656646f6b5a33032deeb361
niki <niki@clustaar.com>
2017-07-05 13:18:51 +0200

    version 0.1

--- influans_putup/simple_recurcive_strategy.py
+++ influans_putup/simple_recurcive_strategy.py
@@ -1,4 +1,13 @@
-import pandas as pd
+import math
+import numpy as np
 
 def simple_recursive_strategy(data):
-    print(data)
+    def srs(worlds, counter):
+        if len(worlds) == 0:
+            return counter
+        return np.max([srs(worlds=possible_worlds(world),
+                           counter=counter+world[0]) for world in worlds])
+    return srs([data], 0)
+
+def possible_worlds(current_list):
+    return [current_list[i:] for i in range(1, 6) if i < current_list.shape[0]]
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import click
 import pandas as pd
+import numpy as np
+import influans_putup.simple_recurcive_strategy as srs
 
 @click.command()
-@click.option('--entity_file', help='csv', required=True)
-@click.option('--model', help='w2v', required=True)
-@click.option('--words', help='csv', required=True)
-@click.option('--out', help='csv', required=False)
+@click.option('--data', help='csv', required=True)
+def simple_recursive_strategy(data):
+    data = pd.read_csv(data, header=None)
+    maximum_reward = srs.simple_recursive_strategy(np.array(data[0]))
+    print(maximum_reward)
